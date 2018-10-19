9c733ae860847bd7eed39a84d56cdf6f9689ebf8
John Alcher <johnalcherdoloiras@gmail.com>
2018-10-19 22:18:49 +0800

    Create Post model and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=3000)
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
