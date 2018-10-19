637c1a9bc97cb99931b8946575dc61156cc5c69e
John Alcher <johnalcherdoloiras@gmail.com>
2018-10-19 16:30:24 +0800

    Render templates instead of HttpResponse

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,41 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'alchermd',
+        'title': 'My first ever post',
+        'body': 'This blog is the best!',
+        'created_at': 'October 19, 2018',
+        'published': True,
+    },
+    {
+        'author': 'janeD',
+        'title': 'Learning Django',
+        'body': 'This framework rocks!',
+        'created_at': 'October 19, 2018',
+        'published': True,
+    },
+    {
+        'author': 'alchermd',
+        'title': 'Why is Python so awesome?',
+        'body': 'The language brings tears to my eyes!',
+        'created_at': 'October 21, 2018',
+        'published': True,
+    },
+    {
+        'author': 'johnD',
+        'title': 'Using the Django ORM',
+        'body': 'This tutorial will rock your socks off.',
+        'created_at': 'October 25, 2018',
+        'published': False,
+    }
+]
 
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    published_posts = [post for post in posts if post.get('published')]
+    return render(request, 'blog/home.djt', {'posts': published_posts})
 
 
 def about(request):
-    return HttpResponse('Hello from the about page!')
+    return render(request, 'blog/about.djt')
